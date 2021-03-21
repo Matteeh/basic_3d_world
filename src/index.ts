5ca049e9ddb173205a7f1385775a3938d191a8d5
Mathias Rahikainen <mathias.rahikainen@gmail.com>
2021-03-21 17:21:13 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,135 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+class BasicWorldDemo {
+  private renderer: THREE.WebGLRenderer;
+  private camera: THREE.PerspectiveCamera;
+  private scene: THREE.Scene;
+  constructor() {
+    this.initialize();
+  }
+
+  private initialize() {
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
+    });
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(this.renderer.domElement);
+
+    window.addEventListener(
+      "resize",
+      () => {
+        this.onWindowResize();
+      },
+      false
+    );
+
+    const fov = 60;
+    const aspect = 1920 / 1080;
+    const near = 1.0;
+    const far = 1000.0;
+    this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    this.camera.position.set(75, 20, 0);
+
+    this.scene = new THREE.Scene();
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1.0);
+    directionalLight.position.set(20, 100, 10);
+    directionalLight.target.position.set(0, 0, 0);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.bias = -0.001;
+    directionalLight.shadow.mapSize.width = 2048;
+    directionalLight.shadow.mapSize.height = 2048;
+    directionalLight.shadow.camera.near = 0.1;
+    directionalLight.shadow.camera.far = 500.0;
+    directionalLight.shadow.camera.near = 0.5;
+    directionalLight.shadow.camera.far = 500.0;
+    directionalLight.shadow.camera.left = 100;
+    directionalLight.shadow.camera.right = -100;
+    directionalLight.shadow.camera.top = 100;
+    directionalLight.shadow.camera.bottom = -100;
+    directionalLight.target = new THREE.Object3D();
+    this.scene.add(directionalLight);
+
+    const ambientLight = new THREE.AmbientLight(0x101010);
+    this.scene.add(ambientLight);
+
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    controls.target.set(0, 20, 0);
+    controls.update();
+    const loader = new THREE.CubeTextureLoader();
+    const texture = loader.load([
+      "./skybox/xpos.png",
+      "./skybox/xneg.png",
+      "./skybox/ypos.png",
+      "./skybox/yneg.png",
+      "./skybox/zneg.png",
+      "./skybox/zpos.png",
+    ]);
+    this.scene.background = texture;
+
+    const plane = new THREE.Mesh(
+      new THREE.PlaneGeometry(100, 100, 10, 10),
+      new THREE.MeshStandardMaterial({
+        color: 0xffffff,
+      })
+    );
+    plane.castShadow = false;
+    plane.receiveShadow = true;
+    plane.rotation.x = -Math.PI / 2;
+    this.scene.add(plane);
+
+    const box = new THREE.Mesh(
+      new THREE.BoxGeometry(2, 2, 2),
+      new THREE.MeshStandardMaterial({
+        color: 0xffffff,
+      })
+    );
+    box.position.set(0, 1, 0);
+    box.castShadow = true;
+    box.receiveShadow = true;
+    this.scene.add(box);
+
+    for (let x = -8; x < 8; x++) {
+      for (let y = -8; y < 8; y++) {
+        const box = new THREE.Mesh(
+          new THREE.BoxGeometry(2, 2, 2),
+          new THREE.MeshStandardMaterial({
+            color: 0x808080,
+          })
+        );
+        box.position.set(
+          Math.random() + x * 5,
+          Math.random() * 4.0 + 2.0,
+          Math.random() + y * 5
+        );
+        box.castShadow = true;
+        box.receiveShadow = true;
+        this.scene.add(box);
+      }
+    }
+
+    this.RAF();
+  }
+
+  private onWindowResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  private RAF() {
+    requestAnimationFrame(() => {
+      this.renderer.render(this.scene, this.camera);
+      this.RAF();
+    });
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const app = new BasicWorldDemo();
+});
